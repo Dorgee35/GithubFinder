70cf934f848cd71cc36f7eaf39e4676ee432de15
Dorgee Lama <DorgeeLama@exactone.com>
2022-01-20 15:55:54 -0700

    Finished profile page

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,86 @@
+// Step 1: We need to import create context
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
+
+// Step 2: Initialize the context
+const GithubContext = createContext();
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+// Step 3: We need a provider that carries our state and data
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  // Get Search results
+  const searchUsers = async (text) => {
+    setLoading();
+    const params = new URLSearchParams({
+      q: text,
+    });
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+    const { items } = await response.json();
+
+    dispatch({
+      type: 'GET_USERS',
+      payload: items,
+    });
+  };
+
+  // Get single user
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    if (response.status === 404) {
+      window.location = '/notfound';
+    } else {
+      const data = await response.json();
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      });
+    }
+  };
+
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
+
+  const clearUsers = () => {
+    dispatch({ type: 'CLEAR_USERS' });
+  };
+
+  // Step 4: we need to return the provider
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        user: state.user,
+        searchUsers,
+        clearUsers,
+        getUser,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+// Step 5: we need to export the context
+export default GithubContext;
---
+++ src/context/github/GithubReducer.js
@@ -0,0 +1,32 @@
+// reducer takes in current state and than an action
+const githubReducer = (state, action) => {
+  // Looking for action type
+  switch (action.type) {
+    case 'GET_USERS':
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    case 'GET_USER':
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      };
+    case 'SET_LOADING':
+      return {
+        ...state,
+        loading: true,
+      };
+    case 'CLEAR_USERS':
+      return {
+        ...state,
+        users: [],
+      };
+    default:
+      return state;
+  }
+};
+
+export default githubReducer;
